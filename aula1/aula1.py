402e73784b78d87febc7b75bc5a1917fc11adef5
Adamszuke <71903897+Adamszuke@users.noreply.github.com>
2024-05-22 22:35:11 -0300

    Add files via upload

---
+++ aula1/aula1.py
@@ -0,0 +1,30 @@
+print("Hello World")
+"Imprime algo - O que for escrito será mostrado no Output"
+
+5 + 10
+"Não vai imprimir porque não foi pedido"
+
+print(3 + 2)
+"Vai imprimir porque foi pedido"
+
+frase = "Big calvo"
+"Cria-se uma variável de texto"
+
+x = 1
+"Cria uma variável de número"
+
+print(frase)
+print(x)
+
+big = False
+
+
+nome = "Gabriel Adamszuke"
+idade = 17
+demolay = "Ativo"
+dinheiro = 136.69
+
+print("Meu nome é: " + nome)
+print("Minha idade é: " + str(idade))
+print("Eu sou: " + demolay)
+print("Eu tenho na minha conta: R$" + str(dinheiro))
